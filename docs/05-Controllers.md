61ebd5a617808f61542bfca3ccd85bbdedb4f203
Jorge Rivera <giorgio1@gmail.com>
2023-09-02 20:28:21 -0600

    SF06. Added controller for books

---
+++ docs/05-Controllers.md
@@ -0,0 +1,6 @@
+#Controllers
+
+The ontrollers are our entry points for the requests we define for the api/app.
+
+By simply using the annotation @Controller, @Service and @Repository, spring has all the necessary information to build the components required to wire them and make them work as a unit
+
---
+++ src/main/java/com/ssde/spring6webapp/controller/BookController.java
@@ -0,0 +1,25 @@
+package com.ssde.spring6webapp.controller;
+
+import com.ssde.spring6webapp.services.BookService;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by jra on 9/2/23-8:08 PM
+ * IDE: IntelliJ IDEA,
+ * Project: bookshelf
+ */
+public class BookController {
+
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model) {
+        model.addAttribute("books", bookService.findAll());
+        return "books";
+    }
+}
